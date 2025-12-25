5d9df2b15de413dfc1ecf00c4f25832bf19a4a36
sriyareddy21 <67216373+sriyareddy21@users.noreply.github.com>
2025-12-25 16:06:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-});
+  plugins: [react()],
+  base: '/Movie_app-Using-API_Key-/',
+})
+
